4157bf4cb43c1ac1428188329310ab7ab6ee0eed
gakkispy <yaosenjun168@live.cn>
2018-09-12 16:37:23 +0000

    topic delete

--- src/utils/util.js
+++ src/utils/util.js
@@ -6,6 +6,18 @@ const diffForHumans = (data, format='YYYYMMDD H:mm:ss') => {
   return moment(data, format).fromNow()
 }
 
+const getCurrentPageUrl = (app) => {
+  // 获取加载的页面
+  let pages = app.getCurrentPages()
+  // 获取当前页面对象
+  let currentPage = pages[pages.length -1]
+  // 当前页面 url
+  let url = currentPage.route
+
+  return url
+}
+
 export default {
-  diffForHumans
+  diffForHumans,
+  getCurrentPageUrl
 }
